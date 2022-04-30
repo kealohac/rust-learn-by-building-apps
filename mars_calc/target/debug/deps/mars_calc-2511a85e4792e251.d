a1a95038bb1f0329863ec49667f4bcf6c817e3cc
Kealoha <kealoha@wecycletoday.com>
2022-04-30 17:11:34 -0600

    Added example, created mars_calc dir

---
+++ mars_calc/target/debug/deps/mars_calc-2511a85e4792e251.d
@@ -0,0 +1,5 @@
+/Users/kealoha/dev/rust/learn-by-building-apps/mars_calc/target/debug/deps/mars_calc-2511a85e4792e251: src/main.rs
+
+/Users/kealoha/dev/rust/learn-by-building-apps/mars_calc/target/debug/deps/mars_calc-2511a85e4792e251.d: src/main.rs
+
+src/main.rs:
---
+++ mars_calc/target/debug/mars_calc.d
@@ -0,0 +1,1 @@
+/Users/kealoha/dev/rust/learn-by-building-apps/mars_calc/target/debug/mars_calc: /Users/kealoha/dev/rust/learn-by-building-apps/mars_calc/src/main.rs
