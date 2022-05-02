44ab30e08b885f3e2e1ca1ce6dc386144cbd3e5c
Kealoha <kealoha@wecycletoday.com>
2022-05-02 17:59:46 -0600

    Compiled server

---
+++ server/target/debug/deps/server-c939e1858e65499c.d
@@ -0,0 +1,5 @@
+/Users/kealoha/dev/rust/learn-by-building-apps/server/target/debug/deps/server-c939e1858e65499c: src/main.rs
+
+/Users/kealoha/dev/rust/learn-by-building-apps/server/target/debug/deps/server-c939e1858e65499c.d: src/main.rs
+
+src/main.rs:
---
+++ server/target/debug/server.d
@@ -0,0 +1,1 @@
+/Users/kealoha/dev/rust/learn-by-building-apps/server/target/debug/server: /Users/kealoha/dev/rust/learn-by-building-apps/server/src/main.rs
