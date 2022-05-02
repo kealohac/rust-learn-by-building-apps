bb2f88871f68eb7523bc13602cdf9503ddc8a71e
Kealoha <kealoha@wecycletoday.com>
2022-05-02 17:58:20 -0600

    Finished mars calculator

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter you weight (kg): ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    println!("Input: {}", input);
+
+    let weight: f32 = input.trim().parse().unwrap();
+    println!("Weight: {}", weight);
+    let mut mars_weight = calculate_weight_on_mars(weight);
+    mars_weight = mars_weight * 1000.0;
+    println!("Weight: {}g", mars_weight);
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
 }
